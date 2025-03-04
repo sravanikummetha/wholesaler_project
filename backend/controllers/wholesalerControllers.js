4007b5b58a202f86992bfdacdd0d727359cb5e91
Sravani Kummetha <sravani.kummetha@tredence.com>
2025-03-04 21:18:27 +0530

    wholesalerdata creation

--- backend/controllers/wholesalerControllers.js
+++ backend/controllers/wholesalerControllers.js
@@ -1,19 +1,36 @@
 const Wholesaler = require("../models/wholesalerModels");
 
 exports.createWholesaler = async (req, res) => {
-    try {
-        const newWholesaler = await Wholesaler.create(req.body);
+  try {
+    const newWholesaler = await Wholesaler.create(req.body);
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newWholesaler
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newWholesaler,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getAllWholesalerData = async (req, res) => {
+  try {
+    const newData = await Wholesaler.find();
+    console.log(newData, "data");
+    res.status(201).json({
+      status: "Success",
+      data: newData,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "fail",
+      message: JSON.stringify(err),
+    });
+  }
 };
--- backend/routes/wholesalerRoutes.js
+++ backend/routes/wholesalerRoutes.js
@@ -4,8 +4,8 @@ const wholesalerController = require("../controllers/wholesalerControllers");
 const router = express.Router();
 
 router
-    .route("/")
-    .post(wholesalerController.createWholesaler)
-
+  .route("/")
+  .post(wholesalerController.createWholesaler)
+  .get(wholesalerController.getAllWholesalerData);
 
 module.exports = router;
