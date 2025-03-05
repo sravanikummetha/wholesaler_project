36b9d277f15cbb1845b7d9f7683d3a263876841e
Sravani Kummetha <sravani.kummetha@tredence.com>
2025-03-05 17:41:14 +0530

    Fetched the get Api in frontend

--- frontend/src/components/common/ReusableTable.jsx
+++ frontend/src/components/common/ReusableTable.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
-const ReusableTable = ({ columns, rows, pageSizeOptions = [5, 10, 15], checkboxSelection = false }) => {
+const ReusableTable = ({
+  columns,
+  rows,
+  pageSizeOptions = [5, 10, 15],
+  checkboxSelection = false,
+}) => {
   return (
     <div className="table-container">
       <DataGrid
         rows={rows}
         columns={columns}
         pageSizeOptions={pageSizeOptions}
+        getRowId={(row) => row.id}
         checkboxSelection={checkboxSelection}
       />
     </div>
